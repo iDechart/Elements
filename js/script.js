4ef51da765ab371c6c912006f000ace9282ebf5a
Elijah <hallbjorn@yandex.ru>
2021-02-19 16:20:44 +0300

    Lesson getElement

---
+++ js/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const box = document.getElementById('box');
+
+console.log(box);
+
+const btns = document.getElementsByTagName('button')[1];
+
+console.log(btns);
+
+const circles = document.getElementsByClassName('circle'); 
+
+console.log(circles);
+
+const hearts = document.querySelectorAll('.heart');
+
+hearts.forEach(item => {
+    console.log(item);
+})
+// console.log(hearts);
+
+const oneHeart = document.querySelector('.heart');
+
+console.log(oneHeart);
+
+ 
